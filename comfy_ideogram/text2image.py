387205f9e05635cba8fd388fa59fa3ccd7e2f0ea
Guan Guan <18370346601@163.com>
2024-09-27 10:25:30 +0800

    update

--- comfy_ideogram/text2image.py
+++ comfy_ideogram/text2image.py
@@ -164,53 +164,6 @@ def pil2tensor(img):
     return output_image, output_mask
 
 
-
-class ColorSelect:
-
-    def __init__(self):
-        pass
-
-    @classmethod
-    def INPUT_TYPES(cls):
-        return {
-            "required": {
-                "color": ("COLOR", {"default": "#FFFFFF"},),
-            },
-            "optional": {
-            }
-        }
-
-    RETURN_TYPES = ("STRING",)
-    RETURN_NAMES = ("value",)
-    FUNCTION = 'picker'
-    CATEGORY = 'Ideogram/select'
-
-    def picker(self, color):
-        ret = color
-        return (ret,)
-
-class Seed_select:
-    @classmethod
-    def INPUT_TYPES(s):
-        return {
-            "required": {
-                "mode": ("BOOLEAN", {"default": True, "label_on": "random", "label_off": "fixed"}),
-                "seed": ("INT", {"default": 0, "min": 0, "max": 0xFFFFFFFFFFFFFFFF}),
-                "fixed_seed": (
-                    "INT",
-                    {"default": 0, "min": 0, "max": 0xFFFFFFFFFFFFFFFF},
-                ),
-            }
-        }
-
-    RETURN_TYPES = ("INT",)
-    RETURN_NAMES = ("seed",)
-    CATEGORY = "Ideogram/seed"
-    FUNCTION = "get_seed"
-
-    def get_seed(self, mode, seed, fixed_seed):
-        return (fixed_seed if not mode else seed,)
-
 class IdeogramTxt2Img:
 
     @classmethod
