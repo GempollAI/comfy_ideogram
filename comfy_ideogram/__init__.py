378d3091d231efc708af79e749e1036daa4ff199
maple@max <feng.liang@kaust.edu.sa>
2024-10-25 18:11:31 +0800

    minor fix

--- comfy_ideogram/__init__.py
+++ comfy_ideogram/__init__.py
@@ -1,9 +1,9 @@
-from .text2image import IdeogramTxt2Img
-from .image2image import IdeogramImg2Img
 from .describe import IdeogramDescribe
+from .edit import IdeogramEdit
+from .helper_nodes import ColorSelect, SeedSelect
+from .image2image import IdeogramImg2Img
+from .text2image import IdeogramTxt2Img
 from .upscale import IdeogramUpscale
-from .helper_nodes import ColorSelect,SeedSelect
-
 
 NODE_CLASS_MAPPINGS = {
     "IdeogramTxt2Img": IdeogramTxt2Img,
@@ -11,18 +11,18 @@ NODE_CLASS_MAPPINGS = {
     "SeedSelect": SeedSelect,
     "IdeogramImg2Img": IdeogramImg2Img,
     "IdeogramDescribe": IdeogramDescribe,
-    "IdeogramUpscale": IdeogramUpscale
-
+    "IdeogramUpscale": IdeogramUpscale,
+    "IdeogramEdit": IdeogramEdit
 }
+
 NODE_DISPLAY_NAME_MAPPINGS = {
     "IdeogramTxt2Img": "IdeogramTxt2Img",
     "ColorSelect": "ColorSelect",
     "SeedSelect": "SeedSelect",
     "IdeogramImg2Img": "IdeogramImg2Img",
     "IdeogramDescribe": "IdeogramDescribe",
-    "IdeogramUpscale": "IdeogramUpscale"
+    "IdeogramUpscale": "IdeogramUpscale",
+    "IdeogramEdit": "IdeogramEdit"
 }
 
-
 all = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
-
